0b4a1a6756ed4c905b15edd6c388c889dd094a7d
Stefan Froemken <froemken@gmail.com>
2018-11-20 17:10:19 +0100

    Move all TCA modifying methods into Overrides

--- ext_tables.php
+++ ext_tables.php
@@ -3,16 +3,14 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'JWeiland.sponsoring',
-    'Sponsoring',
-    'Sponsoring'
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+    'tx_sponsoring_domain_model_project',
+    'EXT:sponsoring/Resources/Private/Language/locallang_csh_tx_sponsoring_domain_model_project.xlf'
+);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+    'tx_sponsoring_domain_model_link',
+    'EXT:sponsoring/Resources/Private/Language/locallang_csh_tx_sponsoring_domain_model_link.xlf'
 );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('sponsoring', 'Configuration/TypoScript', 'Sponsoring');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_sponsoring_domain_model_project', 'EXT:sponsoring/Resources/Private/Language/locallang_csh_tx_sponsoring_domain_model_project.xlf');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_sponsoring_domain_model_project');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_sponsoring_domain_model_link', 'EXT:sponsoring/Resources/Private/Language/locallang_csh_tx_sponsoring_domain_model_link.xlf');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_sponsoring_domain_model_link');
