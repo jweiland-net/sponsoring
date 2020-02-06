92800e06daca4ab7592ae59d8f287e165aaeedd3
Stefan Froemken <froemken@gmail.com>
2020-02-06 15:06:09 +0100

    Set service_bw2 requirement to version 3.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = [
         'depends' => [
             'typo3' => '8.7.0-9.5.99',
             'maps2' => '7.1.3-7.99.99',
-            'service_bw2' => '2.0.0-2.99.99'
+            'service_bw2' => '3.0.0-3.99.99'
         ],
         'conflicts' => [
         ],
