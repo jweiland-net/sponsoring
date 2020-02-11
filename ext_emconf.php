f7ca8365603a8d9e7460f7714d52906afbbc1a3c
Stefan Froemken <froemken@gmail.com>
2020-02-11 09:32:24 +0100

    Update to version 2.0.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,11 +11,11 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '1',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '2.0.2',
+    'version' => '2.0.3',
     'constraints' => [
         'depends' => [
             'typo3' => '8.7.0-9.5.99',
-            'maps2' => '7.1.3-7.99.99',
+            'maps2' => '8.0.0-0.0.0',
             'service_bw2' => '3.0.0-3.99.99'
         ],
         'conflicts' => [
