9b73138dbcd86a77f462b97ce27f6fe262338c28
Stefan Froemken <froemken@gmail.com>
2020-11-16 12:11:00 +0100

    Remove CSH

--- ext_tables.php
+++ ext_tables.php
@@ -3,14 +3,5 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-    'tx_sponsoring_domain_model_project',
-    'EXT:sponsoring/Resources/Private/Language/locallang_csh_tx_sponsoring_domain_model_project.xlf'
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-    'tx_sponsoring_domain_model_link',
-    'EXT:sponsoring/Resources/Private/Language/locallang_csh_tx_sponsoring_domain_model_link.xlf'
-);
-
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_sponsoring_domain_model_project');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_sponsoring_domain_model_link');
