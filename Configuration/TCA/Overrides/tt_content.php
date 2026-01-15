2a0863334eef1b6230a6fa570b95ceaf50dc422c
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2026-01-15 15:45:49 +0100

    [BUGFIX] Add missing TCA configuration for Sponsoring plugin

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -7,6 +7,7 @@
  * LICENSE file that was distributed with this source code.
  */
 
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 if (!defined('TYPO3')) {
@@ -17,4 +18,11 @@ ExtensionUtility::registerPlugin(
     'Sponsoring',
     'Sponsoring',
     'Sponsoring',
+);
+
+ExtensionManagementUtility::addToAllTCAtypes(
+    'tt_content',
+    '--div--;Configuration, pages;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:pages.ALT.list_formlabel,recursive',
+    'sponsoring_sponsoring',
+    'after:subheader',
 );
